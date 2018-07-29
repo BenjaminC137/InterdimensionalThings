4413f7821d5732b1b8f4c3728020cccc812318a4
benjaminc137 <benjamin.salasek@icloud.com>
2018-07-29 13:29:24 -0500

    fixed search bar

--- InterdimensionalThings/Services/SettingsService.cs
+++ InterdimensionalThings/Services/SettingsService.cs
@@ -7,12 +7,11 @@ namespace InterdimensionalThings.Services
         {
             SelectedCurrency = Currencies[0];
             SelectedLanguage = Languages[0];
-
         }
         public string[] Languages = { "English", "Pig Latin", "gazorpian", "Bird Person", "Gromflamish" };
         public string[] Currencies = { "Earth Dollar","Blemflarck", "Brapple","Flurbo","Glem","Schmeckle","Smidgen"};
 
-            public string SelectedLanguage { get; set; }
-            public string SelectedCurrency { get; set; }
+        public string SelectedLanguage { get; set; }
+        public string SelectedCurrency { get; set; }
     }
 }
