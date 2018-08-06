124fa788e8b5f089ec1aaaeb32fd1c6b72e608d9
Joe Johnson <joe.wendell.johnson@gmail.com>
2018-08-06 16:05:14 -0500

    Added scaffolded controller

---
+++ InterdimensionalThings/Controllers/ThingsAdminController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InterdimensionalThings.Data;
+using InterdimensionalThings.Models;
+
+namespace InterdimensionalThings.Controllers
+{
+    public class ThingsAdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ThingsAdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ThingsAdmin
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Things.ToListAsync());
+        }
+
+        // GET: ThingsAdmin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thing = await _context.Things
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (thing == null)
+            {
+                return NotFound();
+            }
+
+            return View(thing);
+        }
+
+        // GET: ThingsAdmin/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ThingsAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,ImagePath,Category,Quality,ShippingDays,Maker,DateCreated,DateLastModified")] Thing thing)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(thing);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thing);
+        }
+
+        // GET: ThingsAdmin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thing = await _context.Things.SingleOrDefaultAsync(m => m.Id == id);
+            if (thing == null)
+            {
+                return NotFound();
+            }
+            return View(thing);
+        }
+
+        // POST: ThingsAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,ImagePath,Category,Quality,ShippingDays,Maker,DateCreated,DateLastModified")] Thing thing)
+        {
+            if (id != thing.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(thing);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ThingExists(thing.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(thing);
+        }
+
+        // GET: ThingsAdmin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var thing = await _context.Things
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (thing == null)
+            {
+                return NotFound();
+            }
+
+            return View(thing);
+        }
+
+        // POST: ThingsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var thing = await _context.Things.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Things.Remove(thing);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ThingExists(int id)
+        {
+            return _context.Things.Any(e => e.Id == id);
+        }
+    }
+}
